0594bffc3b44c3eea44e765a90aa1f330414647e
deokbin <deokbin@naver.com>
2024-12-14 17:35:29 +0900

    feat(Controller): 컨트롤러 전체 흐름 기능 추가

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -3,23 +3,39 @@ import FileReader from '../utils/FileReader.js';
 import { OutputView } from '../View/OutputView.js';
 import { InputView } from '../View/InputView.js';
 import InputValidator from '../utils/InputValidator.js';
+import Attendance from '../Model/Attendance.js';
 
 class Controller {
   static async attendanceSystem(csvPath) {
     const csvData = FileReader.readCSV(csvPath);
     const attendanceData = FileReader.csvToObject(csvData);
-    // console.log(JSON.stringify(attendanceData, null, 2));
-    const currentDate = DateTimes.now();
-    // TOdo: 지금 날짜 반환할 것
-    OutputView.printMessage(`오늘은 12월 (날짜)일 (요일)입니다. 기능을 선택해 주세요.`);
+    const currentDate = new Date(DateTimes.now());
+    let date = currentDate.getDate();
+    const dayList = ['일', '월', '화', '수', '목', '금', '토'];
+    let day = dayList[currentDate.getDay()];
+    while (true) {
+      await this.startSystem(date, day, attendanceData);
+    }
+  }
+
+  static async startSystem(date, day, attendanceData) {
+    const attendance = new Attendance(attendanceData);
+
+    OutputView.printMessage(`오늘은 12월 ${date}일 ${day}요일 입니다. 기능을 선택해 주세요.`);
     const option = await InputView.getInput(`\n
     1. 출석 확인
     2. 출석 수정
     3. 크루별 출석 기록 확인
     4. 제적 위험자 확인
     Q. 종료\n`);
-
-    const validOption = InputValidator.validateOption(option);
+    const validOption = await InputValidator.validateOption(option, date, day);
+    if (validOption === '1') {
+      await attendance.addAttendance();
+    } else if (validOption === '2') {
+      await attendance.fixAttendance();
+    } else if (validOption === '3') {
+      await attendance.checkAttendance();
+    } else if (validOption === 'q' || validOption === 'Q') return false;
   }
 }
 
