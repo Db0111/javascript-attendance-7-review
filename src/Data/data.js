177da5dc62aa1f3b3994bb2ab3bc390cefde5286
deokbin <deokbin@naver.com>
2024-12-14 17:35:40 +0900

    feat(data): 시간, 날짜 관련 데이터 추가

---
+++ src/Data/data.js
@@ -0,0 +1,29 @@
+import { DateTimes } from '@woowacourse/mission-utils';
+
+export const holidays = [1, 7, 8, 14, 15, 21, 22, 25, 28, 29];
+
+export const Days = {
+  월: ['02', '09', '16', '23', '30'],
+  화: ['03', '10', '17', '24', '31'],
+  수: ['04', '11', '18'],
+  목: ['05', '12', '19', '26'],
+  금: ['06', '13', '20', '27'],
+};
+
+export function getDate() {
+  const currentDate = new Date(DateTimes.now());
+  let date = currentDate.getDate();
+  const dayList = ['일', '월', '화', '수', '목', '금', '토'];
+  //수정 필요
+  let day = dayList[currentDate.getDay() - 1];
+
+  return { date, day };
+}
+
+export const educateTime = {
+  월: { startHour: 13, endHour: 18 },
+  화: { startHour: 10, endHour: 18 },
+  수: { startHour: 10, endHour: 18 },
+  목: { startHour: 10, endHour: 18 },
+  금: { startHour: 10, endHour: 18 },
+};
