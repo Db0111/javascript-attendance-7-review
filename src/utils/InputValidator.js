4cc96ac71868357ffa3b3d736a51a0f87cc54943
deokbin <deokbin@naver.com>
2024-12-14 14:44:57 +0900

    feat(InputValidator): 사용자 기능 선택 입력 유효성 검사 클래스 구현

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -1,3 +1,5 @@
+import { holidays } from '../Data/data.js';
+
 class InputValidator {
   static isEmpty(input) {
     if (input === '') {
@@ -6,6 +8,11 @@ class InputValidator {
   }
 
   //Todo: 등교일이 아닌데 출석 확인 할 경우
+  static isNotHoliday(date, day) {
+    if (holidays.includes(date)) {
+      throw new Error(`[ERROR] 12월 ${date}일 ${day}요일은 등교일이 아닙니다.`);
+    }
+  }
 
   static isInvalidOption(input) {
     const options = ['1', '2', '3', '4', 'Q'];
@@ -14,10 +21,26 @@ class InputValidator {
     }
   }
 
-  static validateOption(input) {
+  static isEntrolledName(nickname, attendanceData) {
+    if (!Object.keys(attendanceData).includes(nickname)) {
+      throw new Error(`[ERROR] 등록되지 않은 닉네임입니다.`);
+    }
+  }
+
+  //TOdo: 이미 출석한 경우
+  //   static isAlreadyAdded(nickname, attendanceData) {
+  //     if ()
+  //   }
+
+  static validateOption(input, date, day) {
     this.isEmpty(input);
     this.isInvalidOption(input);
+    this.isNotHoliday(date, day);
     return input;
+  }
+
+  static validateNickName(nickname, attendanceData) {
+    this.isEntrolledName(nickname, attendanceData);
   }
 }
 export default InputValidator;
